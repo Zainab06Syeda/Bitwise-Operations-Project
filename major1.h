ca0541c1ce046aaed3e407efd497e2fc1aa0e4cc
cherry0722 <SnehithaParuchuri@my.unt.edu>
2024-10-29 09:30:44 -0500

    Update major1.h
    Added header part for power.c

--- major1.h
+++ major1.h
@@ -6,5 +6,6 @@ void power_of_2(int num);
 void reverse_bits(int num);
 void replace_bit_position(int num, int mask);
 int check_palindrome(int num);
+int nextPowerOfTwo(int num);
 
 #endif // MAJOR1_H

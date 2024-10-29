98c23b082af0a1f1468c94f51edf9c61be273b68
cherry0722 <SnehithaParuchuri@my.unt.edu>
2024-10-29 10:06:45 -0500

    Create power.c

---
+++ power.c
@@ -0,0 +1,22 @@
+#include "major1.h"  // Replace with the actual header file name
+
+
+// Function to check if a number is a power of two
+int nextPowerOfTwo(int num) {
+    if (num < 1 || num >= 2000000000) {
+        return -1; // Error case for values outside the specified range
+    }
+
+    // Check if num is a power of two using bitwise AND
+    if ((num & (num - 1)) == 0) {
+        return num; // It's already a power of two
+    }
+
+    // If num is not a power of two, find the next power of two
+    int nextPower = 1;
+    while (nextPower <= num) {
+        nextPower <<= 1; // Left shift to double the value
+    }
+
+    return nextPower;
+}

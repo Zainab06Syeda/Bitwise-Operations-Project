96a13deecb2e8df9437b582a8336090e995c0527
veronicafuentesunt <veronicafuentes@my.unt.edu>
2024-10-29 21:50:53 -0500

    Add files via upload

---
+++ major1.h
@@ -0,0 +1,13 @@
+#ifndef MAJOR1_H
+#define MAJOR1_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function prototypes
+void powerOfTwo(int num);
+void reverseBits(int num);
+void replaceBit(int num);
+void checkPalindrome(int num);
+
+#endif // MAJOR1_H
